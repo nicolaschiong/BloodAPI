9c67adeea3e1b6984b875c66bc2bdbaa52d6aac2
Nick <nicolaschiong>
2017-04-04 01:44:34 +0800

    Initial Commit

--- src/main/java/com/nicolaschiong/course/CourseRepository.java
+++ src/main/java/com/nicolaschiong/course/CourseRepository.java
@@ -5,11 +5,13 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.data.rest.core.annotation.RestResource;
 
 /**
  * Created by Nick on 3/20/2017.
  */
+@RepositoryRestResource(exported = false)
 public interface CourseRepository extends PagingAndSortingRepository<Course, Long> {
 
     @RestResource(rel = "title-contains", path = "containsTitle")
--- src/main/java/com/nicolaschiong/review/ReviewRepository.java
+++ src/main/java/com/nicolaschiong/review/ReviewRepository.java
@@ -3,11 +3,13 @@ package com.nicolaschiong.review;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.security.access.prepost.PreAuthorize;
 
 /**
  * Created by Nick on 3/20/2017.
  */
+@RepositoryRestResource(exported = false)
 public interface ReviewRepository extends PagingAndSortingRepository<Review, Long> {
     @Override
     @PreAuthorize("hasRole('ROLE_ADMIN') or @reviewRepository.findOne(#id)?.reviewer?.username == authentication.name")
